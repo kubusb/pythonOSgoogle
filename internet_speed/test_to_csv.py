d70bd154a7628d3d1f5fd3789bebe2e01c8a88fe
Jakub Porebski (ext.) <jakub.porebski@partner.bmwgroup.com>
2023-01-02 12:33:55 +0100

    Unit tests added to measure file

--- internet_speed/test_to_csv.py
+++ internet_speed/test_to_csv.py
@@ -5,35 +5,38 @@ import json
 import os
 import csv
 
-# datetime object containing current date and time
-now = datetime.now()
- 
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%Y-%m-%d %H:%M")
+def write_int_speed_to_csv():
+    # datetime object containing current date and time
+    now = datetime.now()
+    
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%Y-%m-%d %H:%M")
 
-# Let's measure the internet speed
-pomiar = subprocess.run(["fast", "-u", "--single-line", "--json"], capture_output=True)
+    # Let's measure the internet speed
+    pomiar = subprocess.run(["fast", "-u", "--single-line", "--json"], capture_output=True)
 
-# grab the result and decode it
-data  = json.loads(pomiar.stdout.decode())
+    # grab the result and decode it
+    data  = json.loads(pomiar.stdout.decode())
 
-pomiar_download = data["downloadSpeed"]
-pomiar_upload = data["uploadSpeed"]
-pomiar_ping = data["latency"]
+    pomiar_download = data["downloadSpeed"]
+    pomiar_upload = data["uploadSpeed"]
+    pomiar_ping = data["latency"]
 
-csv_row = [dt_string, pomiar_download, pomiar_upload, pomiar_ping]
+    csv_row = [dt_string, pomiar_download, pomiar_upload, pomiar_ping]
 
-# Let's write this data to file
-csv_file = './speedtest.csv'
-isFile = os.path.isfile(csv_file)
+    # Let's write this data to file
+    csv_file = './speedtest.csv'
+    isFile = os.path.isfile(csv_file)
 
-if isFile:
-    with open(csv_file, 'a') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(csv_row)
-else:
-    with open(csv_file, 'w') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames = ["date_time", "dl_speed", "ul_speed", "ping"])
-        writer.writeheader()
-        writer = csv.writer(csv_file)
-        writer.writerow(csv_row)
+    if isFile:
+        with open(csv_file, 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(csv_row)
+    else:
+        with open(csv_file, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames = ["date_time", "dl_speed", "ul_speed", "ping"])
+            writer.writeheader()
+            writer = csv.writer(csv_file)
+            writer.writerow(csv_row)
+
+write_int_speed_to_csv()
---
+++ internet_speed/test_to_csv_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from test_to_csv import write_int_speed_to_csv
+import unittest
+import os
+
+class TestInternet(unittest.TestCase):
+    def test_something(self):
+        path = os.path.exists('./speedtest.csv')
+        assert path.is_file is True
+
+unittest.main()
