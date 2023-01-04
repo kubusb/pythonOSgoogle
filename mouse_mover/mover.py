b56c9c0ac7b75e17c159971ca20ab5260d3354fb
Jakub Porebski (ext.) <Jakub.Porebski@partner.bmwgroup.com>
2023-01-04 19:16:14 +0100

    Mouse mover added to repo

---
+++ mouse_mover/mover.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from time import sleep
+import pyautogui
+while True:
+    pyautogui.moveTo(100, 100, duration = 1)
+    pyautogui.moveTo(200, 200, duration = 1)
+    pyautogui.moveTo(300, 300, duration = 1)
+    pyautogui.moveTo(400, 400, duration = 1)
+    pyautogui.moveTo(500, 500, duration = 1)
+    pyautogui.moveTo(600, 600, duration = 1)
+    pyautogui.moveTo(700, 700, duration = 1)
+    pyautogui.moveTo(800, 800, duration = 1)
+    sleep(10)
