853fc3a2e1492415bfa89256efc501842804a972
Jakub Porebski (ext.) <kubusb@gmail.com>
2023-02-10 17:02:29 +0100

    Job statement added

--- final_exam/week1_image_rotator/rotator.py
+++ final_exam/week1_image_rotator/rotator.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+# Use the Python Imaging Library to do the following to a batch of images:
+# Open an image
+# Rotate an image (90 degrees clockwise)
+# Resize an image (128x128)
+# Save an image in a specific format in a separate directory (.jpeg)
+
