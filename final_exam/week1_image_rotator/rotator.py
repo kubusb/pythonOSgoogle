5bb27a20aba298247a3cf0f66dd6a0c6c0a19660
Jakub Porebski (ext.) <kubusb@gmail.com>
2023-02-10 17:13:05 +0100

    Initial working version

--- final_exam/week1_image_rotator/rotator.py
+++ final_exam/week1_image_rotator/rotator.py
@@ -5,3 +5,10 @@
 # Resize an image (128x128)
 # Save an image in a specific format in a separate directory (/opt/icons/) (.jpeg)
 
+from PIL import Image
+im = Image.open("img/src/_test.jpg")
+print(im.format, im.size, im.mode)
+out128 = im.resize((128, 128))
+out90 = out128.rotate(90)
+out90.save("img/dest/_test.jpeg", "JPEG")
+out90.show()
